a9b942c0c57a501ec3a62975d80d3cc8a5e34204
erickmxav <erickmxav@gmail.com>
2022-04-11 18:11:40 -0300

    fçlkasjflçksajflsj

--- githubAPI/Sources/Application/Scene/Repository/View/RepositoryViewController.swift
+++ githubAPI/Sources/Application/Scene/Repository/View/RepositoryViewController.swift
@@ -25,6 +25,7 @@ class RepositoryViewController: UIViewController {
         tableView.delegate = self
         tableView.register(RepositoryCell.self)
         setupNavBar()
+        fjalsdkçfjlskadfjçlksdjfadls
         
         tableView.rowHeight = UITableView.automaticDimension;
         tableView.estimatedRowHeight = 600;
